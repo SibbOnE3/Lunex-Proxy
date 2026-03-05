9423a60bada5f968e74f457eaacd29b45d1202f6
SibbOnE3 <freddielatham90@gmail.com>
2026-03-05 21:49:06 +0000

    Update index.js

--- public/index.js
+++ public/index.js
@@ -2,28 +2,20 @@
 
 const form = document.getElementById("uv-form");
 const address = document.getElementById("uv-address");
-const searchEngine = document.getElementById("uv-search-engine");
-const error = document.getElementById("uv-error");
-const errorCode = document.getElementById("uv-error-code");
-const loadingRing = document.getElementById("loading");
+const loading = document.getElementById("loading");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  loadingRing.style.display = "block";
-  error.style.display = "none";
-  errorCode.textContent = "";
+  loading.style.display = "block";
 
   try {
     await registerSW();
   } catch (err) {
-    loadingRing.style.display = "none";
-    error.style.display = "block";
-    error.textContent = "Failed to register Service Worker.";
-    errorCode.textContent = err.toString();
+    alert("Engine Registration Failed: " + err.message);
+    loading.style.display = "none";
     throw err;
   }
 
-  const url = search(address.value, searchEngine.value);
+  const url = search(address.value, "https://www.google.com/search?q=%s");
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
