5ec14d3ecca0b699118be483640926ac942ae89b
SibbOnE3 <freddielatham90@gmail.com>
2026-03-05 21:26:16 +0000

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,45 +1,29 @@
-const form = document.getElementById('uv-form');
-const address = document.getElementById('uv-address');
-const searchEngine = document.getElementById('uv-search-engine');
-const error = document.getElementById('uv-error');
-const errorCode = document.getElementById('uv-error-code');
-const loadingRing = document.getElementById('loading');
+"use strict";
 
-// Initialize the secure tunnel connection
-const connection = new BareMux.BareMuxConnection("/baremux/worker.js");
+const form = document.getElementById("uv-form");
+const address = document.getElementById("uv-address");
+const searchEngine = document.getElementById("uv-search-engine");
+const error = document.getElementById("uv-error");
+const errorCode = document.getElementById("uv-error-code");
+const loadingRing = document.getElementById("loading");
 
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    
-    loadingRing.style.display = 'block';
-    error.style.display = 'none';
-    errorCode.textContent = '';
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
-    try {
-        await registerSW();
-    } catch (err) {
-        loadingRing.style.display = 'none';
-        error.style.display = 'block';
-        error.textContent = 'Failed to register Service Worker.';
-        errorCode.textContent = err.toString();
-        throw err;
-    }
+  loadingRing.style.display = "block";
+  error.style.display = "none";
+  errorCode.textContent = "";
 
-    const url = search(address.value, searchEngine.value);
-    let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
-    
-    try {
-        if (await connection.getTransport() !== "/epoxy/index.mjs") {
-            await connection.setTransport("/epoxy/index.mjs", [{ wisp: wispUrl }]);
-        }
-    } catch (err) {
-        loadingRing.style.display = 'none';
-        error.style.display = 'block';
-        error.textContent = 'Failed to connect to the secure tunnel.';
-        errorCode.textContent = err.toString();
-        throw err;
-    }
+  try {
+    await registerSW();
+  } catch (err) {
+    loadingRing.style.display = "none";
+    error.style.display = "block";
+    error.textContent = "Failed to register Service Worker.";
+    errorCode.textContent = err.toString();
+    throw err;
+  }
 
-    // Launch the site
-    window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+  const url = search(address.value, searchEngine.value);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
